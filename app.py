3cdc4e45d0e4cb4e1bc30ea37c101fead8636b40
Aryaman Kukal <aryamankukal@gmail.com>
2020-12-29 15:20:21 -0800

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	templates/index.html

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template, request, url_for, redirect, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_mysqldb import MySQL
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_bootstrap import Bootstrap
-import MySQLdb
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    return render_template('index.html')
+
+@app.route('/uploadAudio', methods=["GET", "POST"])
+def uploadAudio():
     return render_template('index.html')
 
 
