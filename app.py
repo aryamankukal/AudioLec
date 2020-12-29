16f5ae37c4f0b3881aaba3e2a0ee04bda6ca05d0
Aryaman Kukal <aryamankukal@gmail.com>
2020-12-29 13:32:40 -0800

    test commit and push

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
-@app.route("/")
-def home():
-    return render_template('index.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+# this is a comment by aryaman
