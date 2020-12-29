993d0045a13d271dcf5dd9855c5f272779a2b17c
Joel Johnson <jjoel1630@gmail.com>
2020-12-29 13:36:34 -0800

    added app.py back again

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, url_for, redirect, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_mysqldb import MySQL
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bootstrap import Bootstrap
+import MySQLdb
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
