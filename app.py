f607d0dc81abd0c0b2c615048822defcada7afa6
Joel Johnson <jjoel1630@gmail.com>
2020-12-29 15:50:36 -0800

    added flask upload...

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template, request, url_for, redirect, session
+import speech_recognition as sr
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECEIVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('upload.html', transcript=transcript)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
