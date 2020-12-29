117ba552c4014b16fa383b6910b86caed53d6a2e
Aryaman Kukal <aryamankukal@gmail.com>
2020-12-29 15:21:25 -0800

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	templates/index.html

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/uploadAudio', methods=["GET", "POST"])
-def uploadAudio():
-    return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
