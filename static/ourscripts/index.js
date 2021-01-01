facc33062bcd7200a62401fdf620c1e73eac8532
Joel Johnson <jjoel1630@gmail.com>
2021-01-01 11:38:37 -0800

    treid to add audio buttons

--- static/ourscripts/index.js
+++ static/ourscripts/index.js
@@ -1,14 +1,4 @@
 function startRecording() {
     button = document.getElementById('recordingButton')
-    var sound;
-    if(button.classList.contains('Rec')) {
-        console.log("hint");
-        sound = new Audio('/sounds/hint.wav');
-        sound.play()
-    } else {
-        console.log('start');
-        sound = new Audio('/sounds/start.wav');
-        sound.play()
-    }
     button.classList.toggle("Rec")
 }
