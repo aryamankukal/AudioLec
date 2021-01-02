dc9576cec7237b7edf9eff860e31cd4cc96bbcfd
Joel Johnson <jjoel1630@gmail.com>
2021-01-01 16:42:20 -0800

    tried to fix js

--- static/ourscripts/index.js
+++ static/ourscripts/index.js
@@ -1,4 +1,12 @@
 function startRecording() {
-    button = document.getElementById('recordingButton')
+    let button = document.getElementById('recordingButton')
     button.classList.toggle("Rec")
+
+    // console.log(`reached here`);
+    // const button2 = document.createElement("button");
+    // var buttonnode = document.createTextNode("Proceed");
+    // button2.appendChild(buttonnode);
+
+    // var holderdiv = document.getElementById('holder');
+    // holderdiv.appendChild(button2)
 }
