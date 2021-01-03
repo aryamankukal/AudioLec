545c8a3859d94a630186fdeb2b4bd5d43967e0a4
Aryaman Kukal <aryamankukal@gmail.com>
2021-01-03 14:49:26 -0800

    added the emailAnalysis file for emailing the stuff

---
+++ emailAnalysis.py
@@ -0,0 +1,13 @@
+import smtplib
+
+def g(subject, msg, RECIEVER_ADDRESS, PASSWORD, SENDER_ADDRESS):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(SENDER_ADDRESS, PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(SENDER_ADDRESS, RECIEVER_ADDRESS, message)
+        return "Success: Email sent!"
+    except:
+        return "Email failed to send. Try checking your password"
