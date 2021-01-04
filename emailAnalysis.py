c6c64be94a24c0565be2b8a396ba6dd7d6b4be85
Joel Johnson <jjoel1630@gmail.com>
2021-01-03 16:58:20 -0800

    idek

--- emailAnalysis.py
+++ emailAnalysis.py
@@ -1,13 +1,15 @@
 import smtplib
 
-def g(subject, msg, RECIEVER_ADDRESS, PASSWORD, SENDER_ADDRESS):
+
+def send_email(subject, msg, RECIEVER_ADDRESS, PASSWORD, SENDER_ADDRESS):
     try:
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
         server.login(SENDER_ADDRESS, PASSWORD)
-        message = 'Subject: {}\n\n{}'.format(subject, msg)
-        server.sendmail(SENDER_ADDRESS, RECIEVER_ADDRESS, message)
+        message = 'Subject: {} \n\nDear User, \n\n\tHere is your transcript\n\n{}'.format(
+            subject, msg)
+        server.sendmail(SENDER_ADDRESS, RECIEVER_ADDRESS, message, subject)
         return "Success: Email sent!"
     except:
         return "Email failed to send. Try checking your password"
